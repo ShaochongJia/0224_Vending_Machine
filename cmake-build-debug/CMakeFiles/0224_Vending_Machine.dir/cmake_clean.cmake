0ba0806691a8cdbbb766508edf5d3f7b83851238
ShaochongJia <shaochoj@gmail.com>
2022-02-24 14:54:17 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/0224_Vending_Machine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "0224_Vending_Machine"
+  "0224_Vending_Machine.pdb"
+  "CMakeFiles/0224_Vending_Machine.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/0224_Vending_Machine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/0224_Vending_Machine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 0224_Vending_Machine.
+# This may be replaced when dependencies are built.
